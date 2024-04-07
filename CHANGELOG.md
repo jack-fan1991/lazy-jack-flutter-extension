ec17bd99783feda4c83182516e4c04a796147ee7
jack <jack11jack11jack@gmail.com>
2024-04-07 23:21:16 +0800

    # 0.0.97 - Add feature generation clean architecture pattern when right click on folder

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,2 +1,5 @@
+# 0.0.97
+- Add feature generation clean architecture pattern when right click on folder
+
 # 0.0.96
 - feature redirect to local branch when use open github sidebar ui 
--- src/vscode_explorer/flutter/generate_clean_architecture_feature.ts
+++ src/vscode_explorer/flutter/generate_clean_architecture_feature.ts
@@ -2,30 +2,59 @@
 import * as vscode from 'vscode';
 import * as fs from 'fs';
 import * as path from 'path';
+import { get, template } from 'lodash';
+import { toUpperCamelCase } from '../../utils/src/regex/regex_utils';
+import { reFormat } from '../../utils/src/vscode_utils/activate_editor_utils';
 
 const command_clean_architecture = "command_clean_architecture"
 
 export function registerCleanArchitectureGenerate(context: vscode.ExtensionContext) {
     context.subscriptions.push(vscode.commands.registerCommand(command_clean_architecture, async (folderUri) => {
+
         const featureName = await vscode.window.showInputBox({
-            placeHolder: 'Enter feature name'
+            placeHolder: folderUri
         });
 
         if (featureName) {
-            const rootPath = vscode.workspace.workspaceFolders![0].uri.fsPath;
-            const featurePath = path.join(rootPath, 'features', featureName);
+            const rootPath = folderUri.path;
+            const featurePath = path.join(rootPath, featureName);
             try {
                 await fs.mkdirSync(featurePath);
                 fs.mkdirSync(path.join(featurePath, 'data'));
                 fs.mkdirSync(path.join(featurePath, 'domain'));
                 fs.mkdirSync(path.join(featurePath, 'presentation'));
-                fs.mkdirSync(path.join(featurePath, 'presentation', 'view_models'));
+                fs.mkdirSync(path.join(featurePath, 'presentation', 'bloc'));
                 fs.mkdirSync(path.join(featurePath, 'presentation', 'widgets'));
-                fs.writeFileSync(path.join(featurePath, 'homepage.dart'), '');
+                let mainClass = `${featureName}Page`;
+                let dart =path.join(featurePath, `${mainClass}.dart`)
+                fs.writeFileSync(dart, getTemplate(mainClass));
+                //open file
+                const uri = vscode.Uri.file(dart);
+                await vscode.window.showTextDocument(uri);
                 vscode.window.showInformationMessage('Feature created successfully.');
+                reFormat();
             } catch (err) {
                 vscode.window.showErrorMessage('Failed to create feature');
             }
         }
     }));
+}
+
+
+function getTemplate(mainClass: string) {
+let upperCase =toUpperCamelCase(mainClass);
+return `
+import 'package:flutter/material.dart';
+
+class ${upperCase} extends StatelessWidget {
+  const ${upperCase}({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+`
+
 }
